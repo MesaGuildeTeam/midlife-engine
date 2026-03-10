aa16ac9617784943d6e612491dd9117c2d9a1149
Henderythmix <robertoselleswork@gmail.com>
2026-03-10 16:00:49 -0700

    fix: refactored RendererDeferredHL to depend on RendererHL (since it's just a forward+ renderer)

--- assets/hl/default.vert
+++ assets/hl/default.vert
@@ -10,6 +10,7 @@ uniform mat4 u_Camera;
 varying vec3 v_Position;
 varying vec2 v_UV;
 varying vec3 v_Normal;
+varying vec3 v_ScreenNormal;
 varying vec3 v_CameraDir;
 
 void main() {
@@ -17,19 +18,22 @@ void main() {
     float unitZ = 10.0;
 
     vec4 position = u_Transform * vec4(a_Position, 1.0);
-    v_Position = position.xyz;
-    v_CameraDir = normalize((u_Camera * vec4(0.0, 0.0, 1.0, 0.0)).xyz + vec3(v_Position.xy / v_Position.z, 0.0));
+    v_Position = (position).xyz;
 
-    vec4 screen_pos = u_Camera * position;
+    vec4 screen_pos = u_Camera * u_Transform * vec4(a_Position, 1.0);
+
+    v_CameraDir = normalize((u_Camera * vec4(0.0, 0.0, 1.0, 0.0)).xyz + vec3(screen_pos.xy / screen_pos.z, 0.0));
+
     gl_Position = screen_pos;
 
     // perspective distance correction and adding depth
     gl_Position.w = screen_pos.z / (unitZ);
-    gl_Position.z = log(screen_pos.z + 1.0) / log(60.0 + 1.0) * 2.0 - 1.0;
+    gl_Position.z = -log(screen_pos.z + 1.0) / log(60.0 + 1.0);
 
     // Correct width to game screen ratio and add screen space
     gl_Position.xy = gl_Position.xy / vec2(160.0, 120.0) * (pixPerUnit);
 
     v_Normal = (u_Transform * vec4(a_Normal, 0.0)).xyz;
+    v_ScreenNormal = (u_Camera * vec4(v_Normal, 0.0)).xyz * vec3(1.0, 1.0, -1.0);
     v_UV = a_UV;
 }
--- src/recharge/midlife/hl/RendererDeferredHL.hx
+++ src/recharge/midlife/hl/RendererDeferredHL.hx
@@ -12,12 +12,7 @@ import recharge.midlife.base.graphics.RendererAbstract;
 import recharge.midlife.base.Scene;
 import recharge.midlife.base.graphics.Texture;
 
-class RendererDeferredHL extends RendererAbstract {
-  var _vbo:Buffer;
-  var _vao:VertexArray;
-  var _ebo:Buffer;
-
-  var _currentBG:Vec3;
+class RendererDeferredHL extends RendererHL {
 
   var _buffer:FrameBufferHL;
   var _bufferShader:ShaderHL;
@@ -27,163 +22,13 @@ class RendererDeferredHL extends RendererAbstract {
   public function new() {
     super();
 
-    // Setup Rendering Rules
-    GL.enable(GL.DEPTH_TEST);
-    GL.depthFunc(GL.LESS);
-    GL.depthMask(true);
-
-    GL.enable(GL.BLEND);
-    GL.blendFunc(GL.SRC_ALPHA, GL.ONE_MINUS_SRC_ALPHA);
-    GL.enable(GL.CULL_FACE);
-    GL.cullFace(GL.BACK);
-
-    //GL.polygonMode(GL.FRONT_AND_BACK, GL.LINE);
-
-    // Prepare buffers
-    _vbo = GL.createBuffer();
-    _vao = GL.createVertexArray();
-    _ebo = GL.createBuffer();
-
-    GL.bindBuffer(GL.ARRAY_BUFFER, _vbo);
-    GL.bindVertexArray(_vao);
-    GL.bindBuffer(GL.ELEMENT_ARRAY_BUFFER, _ebo);
-
-    // GL.clearDepth(1.0);
-
-    trace("DEBUG: Renderer Initialized");
-
-    // Initialize Shader so we can get attribute locations
-    var _currentShader = cast ShaderHL.defaultShader.getShaderProgram();
-    GL.useProgram(_currentShader);
-    _currentBG = vec3(0.0);
-
-    var atrPos = GL.getAttribLocation(_currentShader, "a_Position");
-    var atrUV = GL.getAttribLocation(_currentShader, "a_UV");
-    var atrNorm = GL.getAttribLocation(_currentShader, "a_Normal");
-
-    if (atrPos != -1) {
-      trace("Identified attribute a_Position");
-      GL.enableVertexAttribArray(atrPos);
-      GL.vertexAttribPointer(atrPos, 3, GL.FLOAT, false, stride, 0);
-    }
-
-    if (atrUV != -1) {
-      trace("Identified attribute a_UV");
-      GL.enableVertexAttribArray(atrUV);
-      GL.vertexAttribPointer(atrUV, 2, GL.FLOAT, false, stride, 3 * 4);
-    }
-
-    if (atrNorm != -1) {
-      trace("Identified attribute a_Normal");
-      GL.enableVertexAttribArray(atrNorm);
-      GL.vertexAttribPointer(atrNorm, 3, GL.FLOAT, false, stride, 5 * 4);
-    }
-
     // Define framebuffer parameters 
     _buffer = new FrameBufferHL();
     _bufferShader = new ShaderHL('midlife/post.frag', 'midlife/post.vert');
-
-    GL.clearColor(0, 0, 0, 1);
-
   }
 
   override public function setBackgroundColor(color:Vec3):Void {
     _currentBG = color;
-  }
-
-  function assignSceneUniforms(shader:Program, scene:Scene) {
-    var ambient = GL.getUniformLocation(shader, "u_Ambient");
-    if (ambient != null) {
-      var v = [_currentBG.x, _currentBG.y, _currentBG.z, 0.0];
-      var v3 = Float32Array.fromArray(v).getData();
-      GL.uniform4fv(ambient, Bytes.fromBytes(v3.bytes), 0, 1);
-    }
-
-    // Point Lights
-    var lightPos = GL.getUniformLocation(shader, "u_LightPos");
-    if (lightPos != null) {
-      var v = Float32Array.fromArray(scene.lights.getLightPosArray()).getData();
-      GL.uniform4fv(lightPos, Bytes.fromBytes(v.bytes), 0, scene.lights.length);
-    }
-
-    var lightColor = GL.getUniformLocation(shader, "u_LightColor");
-    if (lightColor != null) {
-      var v = Float32Array.fromArray(scene.lights.getLightColorArray())
-        .getData();
-      GL.uniform4fv(lightColor, Bytes.fromBytes(v.bytes), 0,
-        scene.lights.length);
-    }
-
-    var lightCount = GL.getUniformLocation(shader, "u_LightCount");
-    if (lightCount != null)
-      GL.uniform1i(lightCount, scene.lights.length);
-
-    // Camera matrix
-    var camPos = GL.getUniformLocation(shader, "u_Camera");
-    if (camPos != null) {
-      var tfArray = new Array<Float>();
-      var camMatrix = scene.camera != null ? scene.camera.transform.inverse() : mat4(1.0);
-      camMatrix.copyIntoArray(tfArray, 0);
-      var camMatrix32 = Float32Array.fromArray(tfArray).getData();
-      GL.uniformMatrix4fv(camPos, false, Bytes.fromBytes(camMatrix32.bytes),
-        0, 1);
-    }
-  }
-
-  override function drawInstruction(instruction:RenderInstruction,
-      scene:Scene) {
-    // Assign shader
-    var currentShader:Program = cast instruction.shader.getShaderProgram();
-    // trace("Using shader program ID " + currentShader);
-    GL.useProgram(currentShader);
-
-    // assign scene uniforms
-    assignSceneUniforms(currentShader, scene);
-
-    // Material Uniforms
-    var tfArray:Array<Float> = new Array();
-    instruction.transformation.copyIntoArray(tfArray, 0);
-    var tf32 = Float32Array.fromArray(tfArray).getData();
-
-    var tfUniform = GL.getUniformLocation(currentShader, "u_Transform");
-    if (tfUniform != null)
-      GL.uniformMatrix4fv(tfUniform, false, Bytes.fromBytes(tf32.bytes), 0, 1);
-
-    // Textures
-    var utUniform = GL.getUniformLocation(currentShader, "u_usesTexture");
-    var diffuse:Null<Texture> = instruction.textures.get(TextureSlot.Diffuse);
-    if (diffuse != null) {
-      // trace("Use texture ID " + diffuse.getTexture());
-      GL.bindTexture(GL.TEXTURE_2D, cast(diffuse.getTexture(), sdl.Texture));
-      GL.bindTexture(GL.TEXTURE0, cast(diffuse.getTexture(), sdl.Texture));
-
-      if (utUniform != null)
-        GL.uniform1i(utUniform, 1);
-    } else {
-      if (utUniform != null)
-        GL.uniform1i(utUniform, 0);
-    }
-
-    var udcUniform = GL.getUniformLocation(currentShader, "u_DiffuseColor");
-    if (udcUniform != null) {
-      var c = instruction.diffuseColor != null ? instruction.diffuseColor : vec4(1,
-        1, 1, 1);
-      var v = Float32Array.fromArray([c.x, c.y, c.z, c.w]).getData();
-      GL.uniform4fv(udcUniform, Bytes.fromBytes(v.bytes), 0, 1);
-    }
-
-    // Bind and draw mesh with shader
-    var vb:Array<Float> = instruction.mesh.getVertices();
-    var vb32 = Float32Array.fromArray(vb).getData();
-    var ib:Array<Int> = instruction.mesh.getIndices();
-    var ib16 = UInt16Array.fromArray(ib).getData();
-
-    GL.bufferData(GL.ARRAY_BUFFER, vb32.byteLength,
-      Bytes.fromBytes(vb32.bytes), GL.DYNAMIC_DRAW);
-    GL.bufferData(GL.ELEMENT_ARRAY_BUFFER, ib16.byteLength,
-      Bytes.fromBytes(ib16.bytes), GL.DYNAMIC_DRAW);
-
-    GL.drawElements(GL.TRIANGLES, ib.length, GL.UNSIGNED_SHORT, 0);
   }
 
   override function preRender(scene:Scene):Void {
@@ -193,10 +38,15 @@ class RendererDeferredHL extends RendererAbstract {
     GL.enable(GL.DEPTH_TEST);
     GL.depthFunc(GL.LESS);
 
+    GL.enable(GL.BLEND);
+    GL.blendFunc(GL.SRC_ALPHA, GL.ONE_MINUS_SRC_ALPHA);
+    GL.enable(GL.CULL_FACE);
+    GL.cullFace(GL.BACK);
+
     var buffersArray = haxe.io.UInt32Array.fromArray([GL.COLOR_ATTACHMENT0, 0x8CE1, 0x8CE2]).getData();
     GL.drawBuffers(3, Bytes.fromBytes(buffersArray.bytes));
     
-    GL.clearColor(0, 0, 0, 1);
+    GL.clearColor(_currentBG.x, _currentBG.y, _currentBG.z, 1);
     GL.clear(GL.COLOR_BUFFER_BIT);
     GL.clear(GL.DEPTH_BUFFER_BIT);
   }
