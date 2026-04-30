abecad1789f69c42e7e73de43ece386dfb998bcb
Henderythmix <robertoselleswork@gmail.com>
2026-04-29 22:38:31 -0700

    feat: integrated window dimensions as a shader parameter

--- src/recharge/midlife/hl/RendererHL.hx
+++ src/recharge/midlife/hl/RendererHL.hx
@@ -7,9 +7,11 @@ import hl.Bytes;
 
 import sdl.GL;
 
+import recharge.midlife.base.Game;
+import recharge.midlife.base.Scene;
+
 import recharge.midlife.base.graphics.RendererAbstract.RenderInstruction;
 import recharge.midlife.base.graphics.RendererAbstract;
-import recharge.midlife.base.Scene;
 import recharge.midlife.base.graphics.Shader;
 import recharge.midlife.base.graphics.Texture;
 
@@ -88,7 +90,15 @@ class RendererHL extends RendererAbstract {
     _currentBG = color;
   }
 
+  
   function assignSceneUniforms(shader:Program, scene:Scene) {
+    var windowDimensions = GL.getUniformLocation(shader, "u_WindowDimensions");
+    if (windowDimensions != null) {
+      var v = [Game.getInstance().dimensions.x, Game.getInstance().dimensions.y, 0.0, 0.0];
+      var v4 = Float32Array.fromArray(v).getData();
+      GL.uniform4fv(windowDimensions, Bytes.fromBytes(v4.bytes), 0, 1);
+    }
+    
     var ambient = GL.getUniformLocation(shader, "u_Ambient");
     if (ambient != null) {
       var v = [_currentBG.x, _currentBG.y, _currentBG.z, 0.0];
